17efe2098d7260e2b4d1dec6e94fd5bf7901acae
haydz <haydnit@gmail.com>
2024-03-18 22:23:03 -0400

    blog completed

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express()
 app.use(bodyParser.json());
@@ -14,9 +15,8 @@ app.get('/posts', (req, res) => {
 
 } );
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
 
+const handleEvent = (type, data) => {
     if (type == 'PostCreated') {
         const { id, title } = data;
 
@@ -42,11 +42,29 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+     console.log("event recieved:", req.body.type);
+
+    handleEvent(type, data);
+
+    // console.log(posts);
     res.send({});
 } );
 
-app.listen(4002, () => {
-    console.log('Listening on 4002')
-});
+app.listen(4002, async () => {
+    console.log("Listening on 4002");
+    try {
+      const res = await axios.get("http://event-bus-clusterip-srv:4005/events");
+   
+      for (let event of res.data) {
+        console.log("Processing event:", event.type);
+   
+        handleEvent(event.type, event.data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }); 
